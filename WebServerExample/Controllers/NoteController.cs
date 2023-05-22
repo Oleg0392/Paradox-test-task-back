4dea06e69d0906e2c5587ac605352e90813f76ed
Хусаинов Олег <khusainovom@m74.local>
2023-05-22 16:37:09 +0500

    Initial commit

---
+++ WebServerExample/Controllers/NoteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebServerExample.Controllers
+{
+    public class NoteController : Controller
+    {
+        Note[] notes =
+        {
+            new Note(0,"note1"),
+            new Note(1,"note2"),
+            new Note(2,"note3"),
+            new Note(3,"note4"),
+            new Note(4,"note5")
+        };
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public JsonResult Index()
+        {           
+            for (int i = 0; i < notes.Length; i++)
+            {
+                notes[i].Raw = "this note written in backend asp.net microservice. #" + i.ToString();
+            }
+            return Json(notes);
+        }
+    }
+}
---
+++ WebServerExample/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace WebServerExample
+{
+    public class Note
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Raw { get; set; } 
+        
+        public Note(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
